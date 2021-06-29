557f29afc243646eac82fb1557c09914d6949fb9
prgnl <alexfilinsky@gmail.com>
2021-06-29 12:58:02 +0200

    gebruikers worden opgehaald via get request

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
-
+Route::get('gebruikers', [App\Http\Controllers\UserController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('user', [App\Http\Controllers\AuthController::class, 'user']);
