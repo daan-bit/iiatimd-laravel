095bd439e6a7330bbf28a6b3cd7e6c54e066ecc6
daan-bit <s1119605@student.hsleiden.nl>
2021-07-05 12:22:58 +0200

    bug fix

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -14,13 +14,6 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => "Admin",
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt("admin"),
-            'rol' => 'werkgever'
-        ]);
-
-        DB::table('users')->insert([
             'name' => "Werknemer",
             'email' => 'werknemer@gmail.com',
             'password' => bcrypt("werknemer"),
@@ -32,6 +25,13 @@ class UserTableSeeder extends Seeder
             'email' => 'werknemer2@gmail.com',
             'password' => bcrypt("werknemer2"),
             'rol' => 'werknemer'
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt("admin"),
+            'rol' => 'werkgever'
         ]);
     }
 }
